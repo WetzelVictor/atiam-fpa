432a32bcd3f720fc564c2b9e2d8881794d84aa1f
WetzelVictor <wetzel.victor@gmail.com>
2017-10-20 15:40:45 +0200

    dict ok

--- testNw.py
+++ testNw.py
@@ -1,11 +1,15 @@
 import nwAlignClass as nw
 import numpy as np
 
-strA = 'GCATGCU'
-strB = 'GATTACA'
+# strA = 'GCATGCU'
+# strB = 'GATTACA'
 # strA = 'GCAT'
 # strB = 'GATT'
 
+strA, strB = 'CEELECANTH', 'PELICAN'
+
 test = nw.nwMatrix(strA, strB)
 
-
+dist = open('atiam-fpa_alpha.dist','w')
+print dist
+print dist[0]
